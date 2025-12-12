3bd92e33acc567abc46f5d2bf1c34aca2db7d37f
Om Animes Sahoo <2405445@kiit.ac.in>
2025-12-12 13:38:06 +0530

    LAB1

---
+++ src/LAB1/P1.java
@@ -0,0 +1,23 @@
+package LAB1;
+
+public class P1 {
+
+    public static void main(String[] args) {
+        String name = "YourName";
+        String rollNo = "YourRollNo";
+        String section = "YourSection";
+        String branch = "YourBranch";
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Section: " + section);
+        System.out.println("Branch: " + branch);
+    }
+
+}
+
+
+
+
+
+
